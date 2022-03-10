de29e5289c859591dc5382b2980efeb8c63f42be
Andrey <bakarbyikov@gmail.com>
2022-03-11 01:33:31 +0500

    minor fixes

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
-from pytube import Search, YouTube
-# from moviepy.editor import AudioFileClip
+from tqdm import tqdm_gui
+from pytube import Search
 from trimmer.trim_source import trim_url
 from trimmer.downloader import extract_youtube_artist_title
 
-SONGS_PATH = r"C:\Users\Pisun\Music\The Zone - Dublin\0"
+SONGS_PATH = r"C:\Users\Pisun\Music\The Zone - Dublin"
 SAVE_PATH = r"C:\Users\Pisun\Documents\condapoj\song_browse\songs"
 
 
 def download_song(url: str, output: str,
                   full_name: str, no_fade: bool = True):
 
-    artist, _, title = extract_youtube_artist_title(url)
-    if not artist or not title:
+    artist, title = extract_youtube_artist_title(url)
+    if not artist or not title or True:
         artist, _, title = full_name.partition(' - ')
     new_file_name = f'{artist} - {title}.mp3'
     new_file_path = os.path.join(output, new_file_name)
@@ -22,12 +22,21 @@ def download_song(url: str, output: str,
              None, None, None, new_file_path)
 
 
-if __name__ == '__main__':
-    songs = filter(lambda x: x.endswith('.mp3'),
-                   os.listdir(SONGS_PATH))
+def main():
+    songs = list(filter(lambda x: x.endswith('.mp3'),
+                        os.listdir(SONGS_PATH)))
 
-    for song in songs:
+    for song in tqdm_gui(songs):
         full_name, _, _ = song.rpartition('.')
         video = Search(full_name).results[0]
         url = video.watch_url
         download_song(url, SAVE_PATH, full_name)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        error = e
+        print("\a")
+        raise e
